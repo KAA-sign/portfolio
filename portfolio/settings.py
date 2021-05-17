4ebaf7c7e126d9a962955de7b1863d73ce4b7f7e
KAA-sign <q89308033587@gmail.com>
2021-05-17 16:30:56 +0300

    add templates & static & config

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'about_me' / 'static',
+    BASE_DIR / 'blog' / 'static',
+]

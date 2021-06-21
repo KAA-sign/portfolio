d5ffdba12752547b1845cda34b38c20f707d8100
KAA-sign <q89308033587@gmail.com>
2021-06-21 16:43:16 +0300

    start html

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index, name='about_me')
+    path('', index, name='about_me') 
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1>Index</h1>')
+    return render(request, 'blog/index.html')

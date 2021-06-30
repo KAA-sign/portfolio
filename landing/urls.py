0dbf2ee3cb2983402cd4d8a7c624e675889e3325
KAA-sign <q89308033587@gmail.com>
2021-06-30 16:29:18 +0300

    chenge app

--- landing/urls.py
+++ landing/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 
 urlpatterns = [
-    path('blog', blog, name='blog') 
+    path('', index, name='about_me') 
 ]
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landing.apps.LandingConfig',
     'blog.apps.BlogConfig',
 ]
 

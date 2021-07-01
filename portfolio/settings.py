9f150d1db6cf6dd0ab91f1a2f5ecf78f2b998bd6
KAA-sign <q89308033587@gmail.com>
2021-07-01 16:37:00 +0300

    add blog

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'landing.apps.LandingConfig',
-    'blog.apps.BlogConfig',
+    'landing',
+    'blog',
 ]
 
 MIDDLEWARE = [
